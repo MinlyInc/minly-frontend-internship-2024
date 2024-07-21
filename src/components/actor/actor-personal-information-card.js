1b87c26e6ce8d87513abdc6d9da2ba9a3a9cf3fa
KevinMarco <kevinmarco9000@gmail.com>
2024-07-21 13:04:15 +0300

    change the name of styles in actor module to be more clear.

--- src/components/actor/actor-personal-information-card.js
+++ src/components/actor/actor-personal-information-card.js
@@ -1,26 +1,26 @@
 import styles from '@/styles/Actor.module.css';
 
 
-const ActorPersonalInformationCard = ({actorInformation}) => {
+const ActorPersonalInformationCard = ({ actorInformation }) => {
   return (
-    <div className={styles.actorInformationCard}>
+    <div className={styles.actorPersonalInformationContainer}>
       <img src={actorInformation.picture} alt="Girl in a jacket" />
 
-      <h2 className={styles.actorInformationHeader}>Personal Information</h2>
-      <text className={styles.actorInformationCardProperty}>Know for</text>
-      <text className={styles.actorInformationCardPropertyValue}>acting</text>
+      <h2 className={styles.actorPersonalInformationHeader}>Personal Information</h2>
+      <text className={styles.actorPersonalInformationCardProperty}>Know for</text>
+      <text className={styles.actorPersonalInformationCardPropertyValue}>acting</text>
 
-      <text className={styles.actorInformationCardProperty}>Gender</text>
-      <text className={styles.actorInformationCardPropertyValue}>{actorInformation.gender}</text>
+      <text className={styles.actorPersonalInformationCardProperty}>Gender</text>
+      <text className={styles.actorPersonalInformationCardPropertyValue}>{actorInformation.gender}</text>
 
 
-      <text className={styles.actorInformationCardProperty}>Birthdate</text>
-      <text className={styles.actorInformationCardPropertyValue}>{actorInformation.birth_date}</text>
+      <text className={styles.actorPersonalInformationCardProperty}>Birthdate</text>
+      <text className={styles.actorPersonalInformationCardPropertyValue}>{actorInformation.birth_date}</text>
 
-      <text className={styles.actorInformationCardProperty}>Nationality</text>
-      <text className={styles.actorInformationCardPropertyValue}>{actorInformation.nationality}</text>
+      <text className={styles.actorPersonalInformationCardProperty}>Nationality</text>
+      <text className={styles.actorPersonalInformationCardPropertyValue}>{actorInformation.nationality}</text>
     </div>
   );
-}
+};
 
 export default ActorPersonalInformationCard;
