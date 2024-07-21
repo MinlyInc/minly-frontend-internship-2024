85b474c9f02124a38b39581df73a6a3fa5df8e3e
KevinMarco <kevinmarco9000@gmail.com>
2024-07-21 13:04:49 +0300

    add name, bio, and rating in actor details

--- src/pages/actors/[uuid]/index.js
+++ src/pages/actors/[uuid]/index.js
@@ -1,42 +1,44 @@
-import uuid from '@/pages/movies/[uuid]';
-import { useRouter } from 'next/navigation'
 import { actor_details_end_point } from '@/constants/end-points';
-import styles from '@/styles/Actor.module.css' ;
+import styles from '@/styles/Actor.module.css';
 import ActorPersonalInformationCard from '@/components/actor/actor-personal-information-card';
+import StarIcon from '@mui/icons-material/Star';
 
-export async function getServerSideProps(context){
-  const {uuid} = context.params ;
-
-  const res = await fetch(
-    `${actor_details_end_point(uuid)}`, {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
+export async function getServerSideProps(context) {
+  const { uuid } = context.params;
+  const res = await fetch(`${actor_details_end_point(uuid)}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   const parsedData = await res.json();
-
-
-  return { props: { actorInformation : parsedData.actor_information , awards: parsedData.awards } }
+  return {
+    props: {
+      actorInformation: parsedData.actor_information,
+      awards: parsedData.awards,
+    },
+  };
 }
 
+const ActorDetailsPage = ({ actorInformation, awards }) => {
+  return (
+    <div className={styles.actorContainer}>
+      <ActorPersonalInformationCard actorInformation={actorInformation} />
 
-const ActorDetailsPage = ({actorInformation , awards}) => {
-    return (
-      <div className={styles.actorContainer} >
-          <ActorPersonalInformationCard actorInformation={actorInformation} />
+      <div className={styles.actorDetailedInformationContainer}>
+        <h1 className={styles.actorDetailedInformationNameProperty}>{actorInformation.name}</h1>
 
-          <div style={{display: 'flex', justifyContent: 'space-between' , flexDirection: 'column'}} >
-            fdsfsafsfsdaffsdhfjskfhjskdhfjskdhfsdjkfhsjkfhskjdsd
-          </div>
+        <div className={styles.actorDetailedInformationRatingContainer}>
+          <StarIcon sx={{ color: 'rgba(254, 182, 0, 1)' }}></StarIcon>
+          <text className={styles.actorDetailedInformationRatingProperty}>{actorInformation.average_rating}</text>
+        </div>
 
-
+        <h2 className={styles.actorDetailedInformationDefaultProperty}>Biography</h2>
+        <p className={styles.actorDetailedInformationBioValue}>{actorInformation.bio}</p>
 
       </div>
-    );
-}
-
+    </div>
+  );
+};
 
 export default ActorDetailsPage;
