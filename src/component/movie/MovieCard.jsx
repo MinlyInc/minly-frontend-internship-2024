6bac8e755c043d3a22fcd3a10093e57525c05840
Karim-Aboudaoud <karimaboudaoud@aucegypt.edu>
2024-07-24 12:55:35 +0300

    finishing story 1

--- src/component/movie/MovieCard.jsx
+++ src/component/movie/MovieCard.jsx
@@ -1,17 +1,24 @@
-
-
 import styles from './MovieCard.module.css';
+import axios from'axios'
+import React, { useEffect } from 'react';
+import StarIcon from '@mui/icons-material/Star';
 
-import React from 'react';
+const MovieReleaseYear = ( releaseDate ) => {
+  const year = new Date(releaseDate).getFullYear();
+  return year;
+};
 
 const MovieCard = ({ movie }) => {
+
+
+
   return (
-    <div className="movie-card">
-      <img src={movie.posterUrl} alt={movie.title} />
-      <div className="movie-info">
-        <h3>{movie.title}</h3>
-        <p>{movie.year}</p>
-        <p>Rating: {movie.rating}</p>
+    <div className={styles.movieCard}>
+      <img src={movie.poster} alt={movie.title} />
+      <div className={styles.movieInfo}>
+        <h3 style={{fontWeight:900}} className="movieInfo-h3">{movie.title} </h3>
+        <p className="movieInfo-p">{MovieReleaseYear(movie.releaseDate)}</p>
+        <p>Rating: {movie.averageRating}</p>
       </div>
     </div>
   );
--- src/component/movie/MovieList.jsx
+++ src/component/movie/MovieList.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import MovieCard from './MovieCard';
 
-const MovieList = ({ movies }) => {
+import MovieCard from './MovieCard';
+import './MovieCard.module.css';
+import axios from'axios'
+import React, { useEffect, useState } from 'react';
+
+
+const MovieList = ({ }) => {
+  const[
+    movies,setmovies
+  ]=useState (null)
+  function getAllMovies(){
+    axios.get(`http://localhost:3001/movies`)
+    .then((res)=>{
+  console.log(res.data);
+      setmovies(res.data)
+    })
+
+    .catch((err)=>{
+  console.log('err',err);
+    })
+  }
+
+  useEffect(()=>{
+    getAllMovies()
+  },[])
   return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+      (movies ?     <div className="movieist">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div> : <></>)
   );
 };
 
